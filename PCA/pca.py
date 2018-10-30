ccd83eb5aa556364f16e8b0536c85fa432910388
ZhengQi，Xu <39328450+Maoracle@users.noreply.github.com>
2018-10-30 10:57:17 +0800

    Add files via upload

---
+++ PCA/pca.py
@@ -0,0 +1,28 @@
+from numpy import *
+
+def loadDataSet(fileName, delim = '\t'):
+    with open(fileName) as fr:
+        stringArr = [line.strip().split(delim) for line in fr.readlines()]
+        datArr = [list(map(float, line)) for line in stringArr]
+        return mat(datArr)
+
+def pca(dataMat, topNfeat = 9999999):
+    meanVals = mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = cov(meanRemoved, rowvar = 0)
+    eigVals, eigVects = linalg.eig(mat(covMat))  #计算特征值和特征向量
+    eigValInd = argsort(eigVals)  #从小到大 提取索引
+    eigValInd = eigValInd[: -(topNfeat + 1) : -1]
+    redEigVects = eigVects[:, eigValInd]
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
+
+def replaceNanWithMean():
+    datMat = loadDataSet('secom.data', ' ')
+    numFeat = shape(datMat)[1]
+    for i in range(numFeat):
+        meanVal = mean(datMat[nonzero(~isnan(datMat[:,i].A))[0],i])
+        datMat[nonzero(isnan(datMat[:,i].A))[0],i] = meanVal
+    return datMat
+
